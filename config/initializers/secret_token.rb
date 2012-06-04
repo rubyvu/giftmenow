66270af3808300969ba1e260d12a7a13ef5dd0f3
Bhanu Prasad <bhanuprasad143@gmail.com>
2012-06-04 10:14:21 +0700

    create giftmenow app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Giftmenow::Application.config.secret_token = '1c112e4d048378b510264da9e371cb169479fbf5086ef1a2a6b4fe608ab812c29ada8a549124b3520a138c119525f35a8329d29f3a3fbdc229468f750f11ab0d'
